7b67bff3d2c030eeb24a5acf994727d1f9a60439
Zach <zmommaerts@gmail.com>
2022-01-11 15:57:43 -0600

    stuck

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
+
+    private
+
+    def unprocessable_entity_response(invalid)
+        render json: { error: git invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
